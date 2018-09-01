93908a0271da3c44479d5dcd5bf38982a5ae7e64
gardad <dante.garda@dell.com>
2018-09-01 12:51:53 -0500

    Proyecto 1

---
+++ PROJECT01_JAVA/src/project01_java/ClaseEjemploNueva.java
@@ -0,0 +1,18 @@
+package project01_java;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gardad
+ */
+public class ClaseEjemploNueva {
+    public int valor=100;
+    public int valor2=200;
+    public void mostrarVariables(){System.out.println("Resultados " +"Valor= "+valor+"    "+"Valor2= "+valor2);}
+    
+}
---
+++ PROJECT01_JAVA/src/project01_java/PROJECT01_JAVA.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project01_java;
+
+/**
+ *
+ * @author gardad
+ */
+public class PROJECT01_JAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+         int edadPoblacion = 200;
+         boolean residenciaMexico = true;
+         System.out.println("Edad "+edadPoblacion);
+         System.out.println("Mexicano? "+residenciaMexico);// TODO code application logic here
+    ClaseEjemploNueva obj1 = new ClaseEjemploNueva ();
+    obj1.valor=obj1.valor+300;
+    obj1.valor2=obj1.valor2+500;
+    obj1.mostrarVariables();
+    
+    
+    }
+    
+}
